e90da6c54f1d18c15f3e8815afe54cc2af40cacf
Felix <felix@iron59.co.uk>
2022-12-13 16:41:57 +0000

    datetime mapping/rouding works!!!!!!!!!

--- process_records_to_db.old.py
+++ process_records_to_db.old.py
@@ -1,16 +1,7 @@
 from db.db import Database
 from datetime import datetime, timedelta
 from typing import Generator
-
-db = Database()
-records = list(db.getAll())
-
-
-# split into half hour chunks
-# get:
-# - average interaction time for each half hour
-# - number of interactions for each half hour
-# - number of contacts handled for each half hour
+from db.cxRecord import CxRecord
 
 
 def generateChunkedDateTimes(
@@ -24,7 +15,10 @@ def generateChunkedDateTimes(
 
 
 def createIndividualChunkData(
-    chunk_size: timedelta, start_date: datetime, end_date: datetime
+    chunk_size: timedelta,
+    start_date: datetime,
+    end_date: datetime,
+    records: Generator[CxRecord, None, None],
 ) -> list[tuple[str, datetime, str, int, int]]:
     """
     Create a list of tuples containing the following data:
@@ -42,29 +36,58 @@ def createIndividualChunkData(
 
     dateTimeChunks = generateChunkedDateTimes(start_date, end_date, chunk_size)
 
+    def mapRecord(
+        record: CxRecord,
+        chunk_size: timedelta,
+    ) -> Generator[tuple[str, datetime, str, int, int], None, None]:
+        for timeChunk in dateTimeChunks:
+            if timeChunk <= record.startTimestamp < timeChunk + chunk_size:
+                # record is in this time chunk
+                yield (
+                    record.queue,
+                    timeChunk,
+                    record.customer,
+                    record.interactionTime,
+                    1,
+                )
+                # records.remove(record)
+                continue
+
+    def mapRecordByRoundingTimestampDownToChunk(
+        record: CxRecord, chunk_size: timedelta
+    ) -> Generator[tuple[str, datetime, str, int, int], None, None]:
+        """Map a record to a time chunk by rounding the timestamp down to the nearest chunk."""
+        # round down record.startTimestamp to nearest chunk_size
+        def roundToChunkSize(timestamp: datetime, chunk_size: timedelta) -> datetime:
+            """Round a timestamp down to the nearest chunk_size."""
+            # 1. convert all stuff to timestamps (seconds since epoch)
+            # 2. get the remainder of the timestamp divided by the chunk size (in seconds)
+            # 3. subtract that remainder from the timestamp
+            # 4. convert back to datetime
+            return datetime.fromtimestamp(
+                timestamp.timestamp()
+                - (timestamp.timestamp() % chunk_size.total_seconds()),
+            )
+
+        yield (
+            record.queue,
+            roundToChunkSize(record.startTimestamp, chunk_size),
+            record.customer,
+            record.interactionTime,
+            1,
+        )
+
     # start a timer (time how long this takes)
     start = datetime.now()
 
     i = 0
-    max_i = 5000
+    max_i = 10000
     for record in records:
         i += 1
         if i > max_i:
             break
-        for timeChunk in dateTimeChunks:
-            if timeChunk <= record.startTimestamp < timeChunk + chunk_size:
-                # record is in this time chunk
-                processedData.append(
-                    (
-                        record.queue,
-                        timeChunk,
-                        record.customer,
-                        record.interactionTime,
-                        1,
-                    )
-                )
-                records.remove(record)
-                continue
+        for chunk in mapRecordByRoundingTimestampDownToChunk(record, chunk_size):
+            processedData.append(chunk)
 
     # end timer
     end = datetime.now()
@@ -79,12 +102,20 @@ def createIndividualChunkData(
     return processedData
 
 
+# split into half hour chunks
+# get:
+# - average interaction time for each half hour
+# - number of interactions for each half hour
+# - number of contacts handled for each half hour
+
+db = Database()
+records = db.getAll()
+
 CHUNK_SIZE = timedelta(minutes=15)
-START_DATE = datetime(2021, 1, 1)
+START_DATE = datetime(2018, 1, 1)
 END_DATE = datetime(2022, 12, 10)
 
-
-chunks = createIndividualChunkData(CHUNK_SIZE, START_DATE, END_DATE)
+chunks = createIndividualChunkData(CHUNK_SIZE, START_DATE, END_DATE, records)
 print(chunks)
 
 # [
